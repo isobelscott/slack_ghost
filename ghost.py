2cf4e73aed246a28eed8158d908772e8fee5b1a4
Isobel Scott <isobel@indeed.com>
2019-11-16 13:11:30 -0500

    best as class & subclass

--- ghost.py
+++ ghost.py
@@ -17,36 +17,64 @@ import time
 # properties
 from configparser import ConfigParser
 
-# use an automated testing driver
-def make_driver(headless=False):
-    moz_driver = "./geckodriver"
 
-    # headless mode
-    if headless == True:
-        options = Options()
-        options.add_argument('-headless')
-        driver = webdriver.Firefox(executable_path=moz_driver, firefox_options=options)
+class Driver(object):
+    """ Driver Instantiates an automated testing driver
+        headless: see the browser for development
+        browser_path: path to actual executable. Download here - https://www.seleniumhq.org/download/
+    """
+    def __init__(self, headless=False, browser_path = "./geckodriver"):
+        self.headless = headless
+        self.browser_path = browser_path
 
-    # browser visible mode
-    driver = webdriver.Firefox(executable_path=moz_driver)
-    return driver
+    def make_driver(self):
+            # headless mode
+        if self.headless == True:
+            options = Options()
+            options.add_argument('-headless')
+            browser = webdriver.Firefox(executable_path=self.browser_path, firefox_options=options)
+        else:
+            # browser visible mode
+            browser = webdriver.Firefox(executable_path=self.browser_path)
+
+        return browser
 
 
-# Login into slack
-def login_account(driver, url, username, password):
-    driver.get(url)
-    send_keys(username + Keys.TAB + password + Keys.TAB)
-    time.sleep(5)
+class SlackGhost(Driver):
+    def __init__(self, url, user, pwd):
+        self.url = url
+        self.user = user
+        self.pwd = pwd
+
+    def login(self):
+        driver = Driver().make_driver()
+        driver.get(self.url)
+
+        username = driver.find_element_by_id("email")
+        username.send_keys(user)
+
+        password = driver.find_element_by_id("password")
+        password.send_keys(pwd)
+
+        #uncheck remember me button
+        driver.execute_script("document.getElementsByClassName"
+                              "('checkbox normal inline_block small_right_margin')[0].click()")
+
+        #signin
+        driver.execute_script("document.getElementById('signin_btn').click()")
+
 
 if __name__ == "__main__":
     config = ConfigParser()
     config.read('config/secrets.properties')
-    username = config.get('SlackSection', 'SLACK_GHOST_USER')
-    password = config.get('SlackSection', 'SLACK_GHOST_PASS')
+    user = config.get('SlackSection', 'SLACK_GHOST_USER')
+    pwd = config.get('SlackSection', 'SLACK_GHOST_PASS')
 
-    url = 'https://testingghost.slack.com'
-    driver = make_driver()
-    login_account(driver, url, username, password)
+    test_url = 'https://testingghost.slack.com'
+
+    SlackGhost(test_url, user, pwd).login()
+
+
 
 
 
